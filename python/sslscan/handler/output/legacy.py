687265e5046f854a2a6beba62c12fc54073775d0
Philipp Seidel <phibo@dinotools.org>
2014-01-08 23:43:46 +0100

    src - Use x509 wrapper to display legacy infos

--- python/sslscan/handler/output/legacy.py
+++ python/sslscan/handler/output/legacy.py
@@ -33,8 +33,32 @@ class Legacy(Output):
                         cipher.get("name", "")
                     )
                 )
+            x509 = host.get("certificate.x509", 0)
             print("")
             print("  SSL Certificate:")
+            print("    Certificate blob:")
+            print(x509.get_certificate_blob())
+            print("    Version: %lu" % x509.get_version())
+            tmp = x509.get_serial_number()
+            print("    Serial Number: %lu (0x%lx)" % (tmp, tmp))
+            print("    Signature Algorithm: %s" % x509.get_signature_algorithm())
+            print("    Issuer: %s" % x509.get_issuer())
+            print("    Not valid before: %s" % x509.get_not_before(3))
+            print("    Not valid after: %s" % x509.get_not_before(3))
+            print("    Subject: %s" % x509.get_subject())
+            print("    Public Key Algorithm: %s" % host.get("certificate.public_key.algorithm", ""))
+            pk_data = host.get("certificate.public_key.data", None)
+            if pk_data is None:
+                print("    Public Key: Could not load")
+            else:
+                pass # ToDo
+
+            print("  Verify Certificate:")
+            verfy_status = host.get("certificate.verify.status", None)
+            if verfy_status:
+                print("    Certificate passed verification")
+            else:
+                print("    %s" % host.get("certificate.verify.error_message", u""))
 
 
 output.register("legacy", Legacy)
