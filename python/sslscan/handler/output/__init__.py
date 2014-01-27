b5fd5ee251e371aed5b18be2fc60281e16b59992
Philipp Seidel <phibo@dinotools.org>
2014-01-27 19:34:24 +0100

    src - Support rule based highlighting

---
+++ python/sslscan/handler/output/__init__.py
@@ -0,0 +1,46 @@
+from sslscan import Color, config as g_config
+
+def get_cipher_color(cipher, rules_name, config=None, color=None):
+    if config == None:
+        config = g_config
+    if color == None:
+        color = Color(config)
+
+    rules = highlight_rules.get("ssllabs-2009e")
+    if rules == None:
+        return ""
+
+    rules = rules.get("rules", {})
+    color_rules = rules.get(rules_name, [])
+    for f in color_rules:
+        c = f(cipher)
+        if c is not None:
+            return getattr(color, c)
+    return ""
+
+def get_cipher_bits_color(cipher, config=None, color=None):
+    return get_cipher_color(cipher, "cipher_bits", config, color)
+
+def get_cipher_method_name_color(cipher, config=None, color=None):
+    return get_cipher_color(cipher, "cipher_method_name", config, color)
+
+def get_cipher_name_color(cipher, config=None, color=None):
+    return get_cipher_color(cipher, "cipher_name", config, color)
+
+highlight_rules = {}
+highlight_rules["ssllabs-2009e"] = {
+    "name": "SSL Labs Rating 2009e",
+    "description": """Highlighting derived from SSL Server Rating Guide version 2009e (21 January 2014) by Qualys SSL Labs http://www.ssllabs.com""",
+    "rules": {
+        "cipher_bits": [
+            lambda cipher: "DANGER" if cipher.get("bits") < 128 else None,
+            lambda cipher: "WARNING" if cipher.get("bits") < 256 else None,
+            lambda cipher: "OK" if cipher.get("bits", 0) >= 256 else None
+        ],
+        "cipher_method_name": [
+            lambda cipher: "DANGER" if cipher.get("method.name") == "SSLv2" else None,
+            lambda cipher: "OK" if cipher.get("method.name") == "TLS12" else None
+        ]
+    }
+}
+
